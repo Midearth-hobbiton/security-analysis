f32b06c6ad312a58a0caf93a3284a37d4f7a072a
Chu Qiao <qc1994qc099@gmail.com>
2023-07-07 10:40:10 -0400

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-// const commit = require('./commit');
+const commit = require('./commit');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
